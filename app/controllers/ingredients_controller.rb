51857ccb13a69d3c066455ed5b12b3398c7de4fe
Lisa Friedman <lisacf@gmail.com>
2013-11-04 19:45:29 -0800

    scope from model to controller

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,5 +1,14 @@
 class IngredientsController < ApplicationController
   def index
-  	@ingredients = Ingredient.all
+  	@ingredients = Ingredient.all.order('name ASC', 'created_at DESC')
+  end
+
+  def destroy
+  	@ingredient = Ingredient.find(params[:id])
+    @ingredient.destroy
+    respond_to do |format|
+      format.html { redirect_to ingredients_url }
+      format.json { head :no_content }
+    end
   end
 end
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,5 +1,4 @@
 class Ingredient < ActiveRecord::Base
 	has_many :proportions
 	has_many :recipes, through: :proportions
-	#default_scope order('name ASC')
 end
